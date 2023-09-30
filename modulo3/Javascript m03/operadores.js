d31955e5fd9d7e9d713c9fdef8ed4d1cf47b32b6
sbi <bustamante20022013@gmail.com>
2023-09-30 18:31:07 -0300

    se añadio el modulo 3 al repositorio

---
+++ modulo3/Javascript m03/operadores.js
@@ -0,0 +1,21 @@
+3 + 5;
+
+console.log(3 + 5);
+
+console.log(14 - 7);
+
+console.log(30 / 9);
+
+console.log(95 % 4);
+console.log(30 % 9);
+
+3 + 5 * 2 - 8
+
+console.log(3 + 5 * 2 - 8);
+
+console.log((3 + 5) * 2 - 8);
+
+console.log("Hola " + "santiago");
+var nombre = "santiago";
+var saludo = "HOLA " + nombre;
+console.log(saludo);
---
+++ modulo3/Javascript m03/variables.js
@@ -0,0 +1,31 @@
+//String
+
+var bootcamp = "henry" ;
+
+//Numeros
+
+var numeroEntero = 18;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.75;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined 
+
+var cajon1;
+
+console.log(cajon1);
+
+//Null
+
+var sinNada = Null;
+
+"String".length //9
+
+var cajon = "Bufanda verde";
